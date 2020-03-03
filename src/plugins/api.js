b4b62dba3013c6f31fecd59e7dd0ae2c2b3435ef
Joming He <joming.he@qq.com>
2020-03-03 17:17:49 +0800

    -

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,5 +1,17 @@
 import Vue from "vue"
 import { $http } from '../../lib'
+import { Message } from 'element-ui'
+
+$http.configure({
+  handle(ret) {
+    if (ret.code != 200) return Promise.reject(new Error(ret.message))
+    return ret
+  },
+  error(err) {
+    Message({ type: 'error', message: err.message })
+    return Promise.reject(err)
+  }
+})
 
 export default Vue.prototype.$api = {
   users(para) { return $http.get('users', para) },
