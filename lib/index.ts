c5bcd79e166765826926935c7c66b601c00bdbf0
Joming He <joming.he@qq.com>
2023-02-20 03:03:35 +0800

    v1.1.10

--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,12 @@ import ExLoading from './loading'
 import ExPagination from './pagination'
  
 // 所有组件列表
-const components = [ ExLoading, ExPagination ]
+const components = [ 
+    { name: 'ex-loading', component: ExLoading }, 
+    { name: 'ex-pagination', component: ExPagination }
+]
 const install = (app: App): void => {    
-    components.map((component) => app.component(component.name, component))
+    components.map(({ name, component }) => app.component(name, component))
 }
  
 export { ExLoading, ExPagination }
