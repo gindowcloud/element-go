dc74284220c2a49a962e3cc7e9d136f6018c39a2
Joming He <joming.he@qq.com>
2023-02-20 13:59:32 +0800

    v1.1.14

--- lib/ExTable/ExTable.vue
+++ lib/ExTable/ExTable.vue
@@ -1,20 +1,39 @@
 <template>
+  <!-- 加载中 -->
+  <ex-loading v-if="!loaded" />
   <!-- 数据区 -->
-  <el-table v-bind="$attrs">
+  <el-table v-else :data="data" v-bind="$attrs" v-loading="loading">
     <slot />
   </el-table>
   <!-- 分页区 -->
-  <ex-pagination v-if="total" :total="total" />
+  <div class="pagination" v-if="total">
+    <ex-pagination :total="total" :page-size="pageSize" :current-page="currentPage" @current-change="currentChange" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ElTable } from 'element-plus'
+import { ref } from 'vue'
+import { ElTable, vLoading } from 'element-plus'
 import ExPagination from '../ExPagination'
 
-defineProps({
-  total: { type: Number, default: 0 }
+const props = defineProps({
+  data: { type: Array, default: () => { return [] } },
+  loaded: { type: Boolean, default: true },
+  loading: { type: Boolean, default: false },
+  total: { type: Number, default: 0 },
+  pageSize: { type: Number, default: 15 },
+  currentPage: { type: Number, default: 1 },
 })
+
+const emit = defineEmits<{
+  (event: 'page-change', payload: number): void
+}>()
+
+const pageSize = ref(props.pageSize)
+const currentPage = ref(props.currentPage)
+const currentChange = (page: number) => emit('page-change', currentPage.value = page)
 </script>
 
 <style scoped>
+.pagination { margin-top: 40px; }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="demo">
-    <div class="title">Element Go</div>
-    <div><ex-loading /></div>
-    <div><ex-page-header back title="文章标题" intro1="好的文章往往从描述开始"><el-button>新建</el-button></ex-page-header></div>
-    <div>
-      <ex-table stripe :data="data" :total="total">
-        <el-table-column prop="id" label="日期" width="260" />
-        <el-table-column prop="name" label="姓名" width="130" />
-        <el-table-column prop="phone" label="电话" />
-        <el-table-column prop="date" label="日期" align="right" />
-      </ex-table>
-    </div>
+    <ex-page-header back title="文章标题" intro1="好的文章往往从描述开始"><el-button>新建</el-button></ex-page-header>
+    <ex-table :data="data" :loaded="loaded" :loading="loading" :total="total" @page-change="getData">
+      <el-table-column prop="id" label="日期" width="260" />
+      <el-table-column prop="name" label="姓名" width="130" />
+      <el-table-column prop="phone" label="电话" />
+      <el-table-column prop="date" label="日期" align="right" />
+    </ex-table>
   </div>
 </template>
 
@@ -18,15 +14,21 @@
 import type { User } from '../types'
 import { ref } from 'vue'
 import { ElButton, ElTableColumn } from 'element-plus'
-import { ExLoading, ExPageHeader, ExTable } from '../../lib'
+import { ExPageHeader, ExTable } from '../../lib'
 import api from '../api'
 
+const loaded = ref(false)
+const loading = ref(false)
 const total = ref<number>(0)
 const data = ref<User[]>([])
 const getData = () => {
+  loading.value = true
   api.users().then((ret: any) => {
     data.value = ret.data
     total.value = ret.meta.pagination.total
+  }).finally(() => {
+    loading.value = false
+    loaded.value = true
   })
 }
 
