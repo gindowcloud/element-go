06a1af20298a55861cfae93f4768a7c1b225c047
Joming He <joming.he@qq.com>
2023-03-03 22:38:45 +0800

    -

--- packages/lib/ExPageHeader/ExPageHeader.vue
+++ packages/lib/ExPageHeader/ExPageHeader.vue
@@ -2,13 +2,13 @@
   <div class="ex-page-header">
     <el-space size="large" class="space" spacer="|">
       <el-button v-if="back" link :icon="ArrowLeft" @click="goBack">返回</el-button>
-      <div>
+      <div class="info">
         <div class="title">{{ title }}</div>
         <div v-if="intro" class="intro" v-html="intro" />
         <div v-else-if="$slots.intro" class="intro"><slot name="intro" /></div>
       </div>
     </el-space>
-    <div v-if="$slots.default && alive" class="links"><slot /></div>
+    <div v-if="$slots.default && alive" class="link"><slot /></div>
   </div>
 </template>
 
@@ -29,7 +29,8 @@ const goBack = () => history.back()
 <style scoped>
 .ex-page-header { display: flex; justify-content: space-between; align-items: center; min-height: 48px; padding-bottom: 12px; margin-bottom: 40px; border-bottom: 1px solid #eee; }
 .ex-page-header .back { font-size: 15px; }
-.ex-page-header .title { font-size: 20px; font-weight: 600; }
-.ex-page-header .intro { font-size: 14px;  color: #ccc; }
-.ex-page-header .links { position: relative; }
+.ex-page-header .info { line-height: 1.5; }
+.ex-page-header .info .title { font-size: 20px; font-weight: 600; }
+.ex-page-header .info .intro { font-size: 14px;  color: #ccc; }
+.ex-page-header .link { position: relative; }
 </style>
