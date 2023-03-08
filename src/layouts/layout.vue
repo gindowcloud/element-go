3d733938c28cf2d388c241372c42d405849c7652
Joming He <joming.he@qq.com>
2023-03-09 00:50:01 +0800

    -

--- src/layouts/layout.vue
+++ src/layouts/layout.vue
@@ -12,11 +12,11 @@ import { HamburgerButton, Search, TableFile, Loading, MoreFour, Page } from '@ic
 import menu from './menu'
 
 const channels: Array<Menu> = [
+  { title: '布局', icon: Page, path: '/components/layout' },
   { title: '页头', icon: HamburgerButton, path: '/components/page-header' },
   { title: '搜索', icon: Search, path: '/components/form-search' },
   { title: '表格', icon: TableFile, path: '/components/table' },
   { title: '加载', icon: Loading, path: '/components/loading' },
   { title: '分页', icon: MoreFour, path: '/components/pagination' },
-  { title: '布局', icon: Page, path: '/components/layout' }
 ]
 </script>
--- src/layouts/menu.ts
+++ src/layouts/menu.ts
@@ -2,12 +2,12 @@ import type { Menu } from '../../packages/lib/types'
 import { HamburgerButton, Search, TableFile, Loading, MoreFour, Page } from '@icon-park/vue-next'
 
 const menu: Array<Menu> = [
+  { title: 'ExLayout 布局', icon: Page, path: '/components/layout' },
   { title: 'ExPageHeader 页头', icon: HamburgerButton, path: '/components/page-header' },
   { title: 'ExSearch 搜索', icon: Search, path: '/components/form-search' },
   { title: 'ExTable 表格', icon: TableFile, path: '/components/table' },
   { title: 'ExLoading 加载', icon: Loading, path: '/components/loading' },
   { title: 'ExPagination 分页', icon: MoreFour, path: '/components/pagination' },
-  { title: 'ExLayout 布局', icon: Page, path: '/components/layout' }
 ]
 
 export default menu
