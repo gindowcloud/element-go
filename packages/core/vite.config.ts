751105b555c0669594ac4a4b6ac30c906d6127f3
Gindow Cloud <joming.he@qq.com>
2024-10-14 00:07:14 +0800

    -

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'element-go'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: { globals: { vue: 'Vue' } }
     }
   }
 })
