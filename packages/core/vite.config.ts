bf8d94a631e8ca55bb63a0aaebc23cb6e63e2d53
Gindow Cloud <joming.he@qq.com>
2024-10-14 16:22:30 +0800

    -

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,16 +1,21 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts()
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'element-go'
+    },
+    rollupOptions: {
+      external: ['vue', 'vue-router']
     }
   }
 })
